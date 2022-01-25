42e1f3cddc0e0e1ab06ded45eb4f420ab5de1347
karakumium <t-q-l@ya.ru>
2022-01-25 13:56:04 +0300

    lab9

--- Lab9/main.tf
+++ Lab9/main.tf
@@ -14,5 +14,47 @@ resource "aws_db_instance" "lab9-prod" {
   skip_final_snapshot  = true
   apply_immediately    = true
   username             = "administrator"
-  password             = "fuckthepassword"
+  password             = data.aws_ssm_parameter.rds_password.value
+}
+//generate password
+resource "random_password" "lab9-db-pass" {
+  length           = 24
+  special          = true
+  override_special = "#!()-_"
+}
+
+resource "aws_ssm_parameter" "rds_password" {
+  name        = "/prod/lab9-prod-mysql-rds/password"
+  description = "master password for RDS"
+  type        = "SecureString"
+  value       = random_password.lab9-db-pass.result
+}
+
+//retrieve password
+
+data "aws_ssm_parameter" "rds_password" {
+  name       = "/prod/lab9-prod-mysql-rds/password"
+  depends_on = [aws_ssm_parameter.rds_password]
+}
+
+//outputs
+
+output "lab9-rds_address" {
+  value = aws_db_instance.lab9-prod.address
+}
+
+output "lab9-rds_port" {
+  value = aws_db_instance.lab9-prod.port
+}
+
+output "lab9-rds_endpoint" {
+  value = aws_db_instance.lab9-prod.endpoint
+}
+output "lab9-rds_username" {
+  value = aws_db_instance.lab9-prod.username
+}
+output "lab9-rds_password" {
+  value = nonsensitive(data.aws_ssm_parameter.rds_password.value)
+
+  // sensitive = true
 }
